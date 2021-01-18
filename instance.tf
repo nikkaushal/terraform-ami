ee12ed687d689c2c2f220f253560d69095af83b9
nikkaushal <nikkaushal@gmail>
2021-01-17 23:53:41 -0500

    ami

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ami_instance" {
   ami                   = data.aws_ami.ami.id
   instance_type         = "t3.small"
   tags = {
-    name                = "${var.COMPONENT}-ami"
+    name                = "front-ami"
   }
 
 }
