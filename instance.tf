98219df583961f61dcb7d29a7187f8efde37e32f
nikkaushal <nikkaushal@gmail>
2021-01-18 12:21:46 -0500

    ami

--- instance.tf
+++ instance.tf
@@ -48,3 +48,9 @@ resource "null_resource" "provisioner" {
     ]
   }
 }
+
+resource "aws_ami_from_instance" "ami" {
+  depends_on = [null_resource.provisioner]
+  name                   = var.COMPONENT
+  source_instance_id     = aws_instance.ami_instance.id
+}
