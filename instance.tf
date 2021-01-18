214cda1bc2b8e6860d36f8d2215b9aebcd75f927
nikkaushal <nikkaushal@gmail>
2021-01-18 14:30:59 -0500

    ami

--- instance.tf
+++ instance.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow-ssh" {
   ingress {
     description       = "SSH"
     from_port         = 22
-    to_port           = 443
+    to_port           = 22
     protocol          = "tcp"
     cidr_blocks       = ["0.0.0.0/0"]
   }
