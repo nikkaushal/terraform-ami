992166e2a68d69bee15864a929dfee2bfeb373cc
nikkaushal <nikkaushal@gmail>
2021-04-05 08:24:47 -0400

    terraform-ami

--- data.tf
+++ data.tf
@@ -1,6 +1,12 @@
 data "aws_ami" "ami" {
   most_recent         = true
   owners              = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Centos-7-DevOps-Practice"]
+  }
+
 }
 
 data "aws_secretsmanager_secret" "secrets" {
